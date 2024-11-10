727dd617ceb4b13414116b1a83a2f0dfa766ec5d
Lehlohonlo Botji <lbotji@gmail.com>
2024-11-10 20:31:43 +0200

    added city function and updated search function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+function displayTemperature(response) {
+  
+}
+
+
+function showCity(city) {
+let city = "Paris";
+let apiKey = dac27f8d3491eb544bc74fd800f1to5c;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+axios.get(apiUrl).then(displayTemperature);
+}
+
+
+
 function searchSubmit (event) {
   event.preventDefault();
  let searchInput = document.querySelector("#search-input");
