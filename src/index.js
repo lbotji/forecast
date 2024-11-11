ee5b536f3ff100f73ff4e010e89f55504e37cdaa
Lehlohonlo Botji <lbotji@gmail.com>
2024-11-11 09:12:36 +0200

    added a new id in html. updated js with a function to change temperature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value")
+  let temperature = response.data.current.temperature;
+  let cityElement = document.querySelector("city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
   
 }
 
 
 function showCity(city) {
-let city = "Paris";
 let apiKey = dac27f8d3491eb544bc74fd800f1to5c;
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
 axios.get(apiUrl).then(displayTemperature);
@@ -15,8 +20,8 @@ axios.get(apiUrl).then(displayTemperature);
 function searchSubmit (event) {
   event.preventDefault();
  let searchInput = document.querySelector("#search-input");
- let cityElement = document.querySelector("#city");
- cityElement.innerHTML = searchInput.value; 
+ 
+ showCity = (searchInput.value); 
 }
 
 let searchFormElement = document.querySelector("#search-form");
