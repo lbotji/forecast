00e452f95bab1f6aa95e50419c6060c0713b2c7e
Lehlohonlo Botji <lbotji@gmail.com>
2024-11-11 22:58:06 +0200

    added some variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-function displayTemperature(response) {
+function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
@@ -12,7 +13,7 @@ function displayTemperature(response) {
 function showCity(city) {
 let apiKey = dac27f8d3491eb544bc74fd800f1to5c;
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayWeather);
 
 }
 
