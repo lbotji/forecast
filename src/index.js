84006d91b1dd88cf87d5e0a10667253faedc14c5
Lehlohonlo Botji <lbotji@gmail.com>
2024-11-11 23:36:46 +0200

    edited and injected more functions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let currentDate = new Date (response.data.time * 1000);
 
-
-  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+
   
 }
 
@@ -21,7 +39,7 @@ axios.get(apiUrl).then(displayWeather);
 
 function searchSubmit (event) {
   event.preventDefault();
- let searchInputElement = document.querySelector("#search-input");
+ let searchInput = document.querySelector("#search-input");
  
  showCity = (searchInput.value); 
 }
