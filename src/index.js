f622defc0a3c5bd7b6196eef47224a124d595e18
Lehlohonlo Botji <lbotji@gmail.com>
2024-11-19 14:58:28 +0200

    dejected from the forcast

--- src/index.js
+++ src/index.js
@@ -66,9 +66,10 @@ function displayForecast(response) {
   let forecastHtml = "";
 
   days.forEach(function(day) {
-   forecastHtml = forecastHtml + `<div class="weather-forecast-day">
+   forecastHtml = forecastHtml +
+    `<div class="weather-forecast-day">
       <div class="weather-forecast-date">${day}.</div>
-      <div class="weather-forecast-icon">🌤</div>
+      <div class = "weather-forecast-icon">🌤</div>
       <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
     <strong>15°</strong></div>
