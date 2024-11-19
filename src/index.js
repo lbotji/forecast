8f5b956c53470c40805b9ec387529f14ff8c4133
Lehlohonlo Botji <lbotji@gmail.com>
2024-11-19 14:12:38 +0200

    injected a function for getting the api forecast and another for responding to change the days

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function displayWeather(response) {
   let iconElement = document.querySelector("#icon");
 
 
-
+  
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-icon"  />`;
   cityElement.innerHTML = response.data.city;
@@ -18,6 +18,7 @@ function displayWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -45,7 +46,7 @@ axios.get(apiUrl).then(displayWeather);
 
 }
 
-function searchSubmit (event) {
+function searchSubmit(event) {
   event.preventDefault();
  let searchInput = document.querySelector("#search-input");
  
@@ -55,6 +56,33 @@ function searchSubmit (event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
 
+
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function(day) {
+   forecastHtml = forecastHtml + `<div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}.</div>
+      <div class="weather-forecast-icon">🌤</div>
+      <div class="weather-forecast-temperatures">
+  <div class="weather-forecast-temperature">
+    <strong>15°</strong></div>
+  <div class="weather-forecast-temperature">9°</div>
+    </div>
+     </div>`; 
+
+});
+}
+
+function getForecast(city) {
+  let apiKey = "dac27f8d3491eb544bc74fd800f1to5c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&unit=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 
 showCity("Lisbon");
